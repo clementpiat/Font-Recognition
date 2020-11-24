9c79e83344d584a90c4adc2b9738b5fe7cb37973
Quentin Blampey <quentinblampey@MacBook-Pro-de-Quentin.home>
2020-11-24 18:46:46 +0100

    now screen on a single text

--- scrapping.py
+++ scrapping.py
@@ -1,4 +1,9 @@
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 import json
 from time import time
 import os
@@ -9,6 +14,7 @@ with open('config.json') as json_file:
 driver = webdriver.Chrome()
 
 fonts = ["Roboto"]
+css_selector = '.variant__font-render'
 
 for font in fonts:
     folder_path = f"{config['data_path']}/{font}"
@@ -17,6 +23,15 @@ for font in fonts:
         os.mkdir(folder_path)
 
     driver.get(f'https://fonts.google.com/specimen/{font}')
-    driver.save_screenshot(f"{folder_path}/{time()}.png")
+
+    try:
+        element_present = EC.presence_of_element_located((By.CSS_SELECTOR, css_selector))
+        WebDriverWait(driver, config["timeout"]).until(element_present)
+    except TimeoutException:
+        print("Timed out waiting for page to load")
+    finally:
+        print("Page loaded")
+        element = driver.find_elements_by_css_selector(css_selector)[0]
+        element.screenshot(f"{folder_path}/{time()}.png")
 
 driver.close()
