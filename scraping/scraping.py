61047b6e996e86e5bc902d6f58a0c65f23bcab9e
Clément Piat <clement.piat2@gmail.com>
2020-12-01 12:06:51 +0100

    scraping: add font style parameter

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from essential_generators import DocumentGenerator
 
 import json
-from time import time
+from time import time, sleep
 import os
 from tqdm import tqdm
 
@@ -22,7 +22,10 @@ if not config["chromedriver_path"]:
 else:
     driver = webdriver.Chrome(config["chromedriver_path"])
 
-css_selector = '.variant__font-render'
+font_selector = '.variant__font-render'
+style_selector = '.variant__style'
+style = 'Regular 400'
+sleep_time = 0.5 # Time of sleeping between scrolling and screenshot (in seconds).
 
 for font in tqdm(fonts):
     folder_path = f"{config['data_path']}/{font}"
@@ -36,7 +39,8 @@ for font in tqdm(fonts):
         driver.get(f'https://fonts.google.com/specimen/{font}?preview.text={text}&preview.text_type=custom')
 
         try:
-            element_present = EC.presence_of_element_located((By.CSS_SELECTOR, css_selector))
+            # Wait for the first font style to be loaded, we assume that every style load almost at the same time.
+            element_present = EC.presence_of_element_located((By.CSS_SELECTOR, font_selector))
             WebDriverWait(driver, config["timeout"]).until(element_present)
         except TimeoutException:
             print("Timed out waiting for page to load")
@@ -44,8 +48,19 @@ for font in tqdm(fonts):
             # print("Page loaded")
             # Additional safety
             try:
-                element = driver.find_elements_by_css_selector(css_selector)[0]
-                element.screenshot(f"{folder_path}/{time()}.png")
+                elements = driver.find_elements_by_css_selector(".variant")
+                styles = [el.find_element_by_css_selector(style_selector).text for el in elements]
+                if style in styles:
+                    style_index = styles.index(style)
+                else:
+                    break
+
+                element = driver.find_elements_by_css_selector(font_selector)[style_index]
+                # Take the screenshot
+                height = driver.get_window_size()["height"]
+                driver.execute_script(f"window.scrollTo(0, {element.location['y'] - height//2});")
+                sleep(sleep_time)
+                element.screenshot(f"{folder_path}/{time()}.png")  
             except:
                 print("Error while trying to screenshot...")
 
