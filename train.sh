9f9c4315de85328856604c0b7804a12c668d7fbe
Clément Piat <clement.piat2@gmail.com>
2020-12-10 09:54:28 +0100

    train.sh: fix

--- train.sh
+++ train.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-#PBS -S /bin/bash
-#PBS -N train
-#PBS -j oe
-#PBS -l walltime=02:00:00
-#PBS -l select=1:ncpus=8:mem=8gb
-#PBS -P gr
-#PBS -M clement.piat@student.ecp.fr
+#SBATCH --job-name=train
+#SBATCH --output=%x.o%j
+#SBATCH --ntasks=1
+#SBATCH --time=01:00:00
+#SBATCH --partition=cpu_short
+#SBATCH --mem=16gb
+#SBATCH --cpus-per-task=8
 
-# Load necessary modules
+# To clean and load modules defined at the compile and link phases
 module purge
-module load anaconda3/5.3
+module load anaconda3/2020.02/gcc-9.2.0
 
 # Activate anaconda environment
 source activate /gpfs/users/piatc/.conda/envs/font
 
 # Go to the directory where the job has been submitted 
-cd $PBS_O_WORKDIR
+cd ${SLURM_SUBMIT_DIR}
 
 # Execution
 python training.py
