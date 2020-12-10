24aa29416a870494581e0b94cc06770cc9dfcd87
Clément Piat <clement.piat2@gmail.com>
2020-12-10 09:35:25 +0100

    training: argparse

--- training.py
+++ training.py
@@ -1,3 +1,5 @@
+import argparse
+import json
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -9,57 +11,69 @@ from torch.nn import functional as F
 from learning.dataset import FontDataset
 from model import Model
 
-batch_size = 64
-epochs = 2
-train_size = 0.7
+def main(batch_size, epochs, train_size, dataset):
+    dataset = FontDataset(dataset, siamese=True)
 
-dataset = FontDataset("example", siamese=True)
+    train_size = int(train_size * len(dataset))
+    test_size = len(dataset) - train_size
+    train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
 
-train_size = int(train_size * len(dataset))
-test_size = len(dataset) - train_size
-train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
-
-for data in train_loader:
-    img1, img2, label = data
-    break
-
-model = Model(img1.shape[3], img1.shape[2])
-optimizer = Adam(model.parameters(), lr=1e-3)
-
-def bce_loss(prediction, label):
-    return F.binary_cross_entropy(prediction, label, reduction="sum")
-
-# Train
-for epoch in range(epochs):
-    train_loss = 0
     for data in train_loader:
+        img1, img2, label = data
+        break
+
+    model = Model(img1.shape[3], img1.shape[2])
+    optimizer = Adam(model.parameters(), lr=1e-3)
+
+    def bce_loss(prediction, label):
+        return F.binary_cross_entropy(prediction, label, reduction="sum")
+
+    # Train
+    for epoch in range(epochs):
+        train_loss = 0
+        for data in train_loader:
+            img1, img2, label = data
+            img1, img2, label = img1.type(torch.FloatTensor), img2.type(torch.FloatTensor), label.type(torch.FloatTensor)
+
+            optimizer.zero_grad()
+            
+            prediction = model(img1, img2)
+            loss = bce_loss(prediction, label)
+
+            loss.backward()
+            train_loss += loss.item()
+            optimizer.step()
+
+        print('====> Epoch: {} Average loss: {:.4f}'.format(
+            epoch, train_loss / len(train_loader.dataset)))
+
+
+    # Test
+    predictions, labels = [], []
+    for data in test_loader:
         img1, img2, label = data
         img1, img2, label = img1.type(torch.FloatTensor), img2.type(torch.FloatTensor), label.type(torch.FloatTensor)
 
-        optimizer.zero_grad()
-        
-        prediction = model(img1, img2)
-        loss = bce_loss(prediction, label)
-
-        loss.backward()
-        train_loss += loss.item()
-        optimizer.step()
-
-    print('====> Epoch: {} Average loss: {:.4f}'.format(
-          epoch, train_loss / len(train_loader.dataset)))
+        predictions += list(model(img1, img2).detach().numpy())
+        labels += list(label.detach().numpy())
 
 
-# Test
-predictions, labels = [], []
-for data in test_loader:
-    img1, img2, label = data
-    img1, img2, label = img1.type(torch.FloatTensor), img2.type(torch.FloatTensor), label.type(torch.FloatTensor)
+    print(f"\n> Test accuracy: {1 - np.mean(np.abs(np.array(labels)-np.round(predictions)))}")
 
-    predictions += list(model(img1, img2).detach().numpy())
-    labels += list(label.detach().numpy())
-
-print(labels, predictions)
-print(f"Test accuracy: {1 - np.mean(np.abs(np.array(labels)-np.array(predictions)))}")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--batch_size", type=int, default=64, 
+        help="dataset batch size")
+    parser.add_argument("-e", "--epochs", type=int, default=2, 
+        help="number of epochs")
+    parser.add_argument("-t", "--train_size", type=float, default=0.7, 
+        help="dataset train size")
+    parser.add_argument("-d", "--dataset", type=str, default="example", 
+        help="dataset name in the data folder")
+    args = parser.parse_args()
+    print(f"\n> args:\n{json.dumps(vars(args), sort_keys=True, indent=4)}\n")
+    
+    main(args.batch_size, args.epochs, args.train_size, args.dataset)
