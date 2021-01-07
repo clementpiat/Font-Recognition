660aae197238c42bfc88f1b3b0eb8c70b44b94e6
Clément Piat <clement.piat2@gmail.com>
2021-01-07 16:19:48 +0100

    model: new try

--- model.py
+++ model.py
@@ -23,8 +23,7 @@ class Model(nn.Module):
             nn.Linear(self.d_conv, 512),
             nn.Dropout(inplace=True),
             nn.ReLU(inplace=True),
-            nn.Linear(512, 128),
-            nn.Sigmoid()
+            nn.Linear(512, 128)
         )
 
         self.feed_forward = nn.Sequential(
